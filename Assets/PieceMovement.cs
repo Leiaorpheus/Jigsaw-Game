a0698e6a8874501a1d8eb74547f4b99c9b2c32fa
Ophelia Huang <leiahuang@outlook.com>
2018-10-11 19:08:46 +0100

    basic movement

---
+++ Assets/PieceMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovement : MonoBehaviour {
+
+    public bool placed;
+	
+	// Update is called once per frame
+	void Update () {
+
+        // move the puzzle with mouse movement
+        if (!placed)
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+        
+	}
+
+    // if it fits in one socket
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == gameObject.name)
+        {
+            transform.position = other.gameObject.transform.position;
+            placed = true;
+        }
+    }
+}
