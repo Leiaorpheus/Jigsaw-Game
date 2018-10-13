b762c96727faf40a6052aac881f210d9789a2b3b
Ophelia Huang <leiahuang@outlook.com>
2018-10-13 15:50:24 +0100

    scrolling inventory

---
+++ Assets/ReturnPiece.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPiece : MonoBehaviour {
+    public KeyCode returnPiece;
+
+   
+    public Vector2 piecePosition;
+
+	
+    // putting puzzle back to inventory
+    public void returnPuzzle()
+    {
+        if (Input.GetKeyDown(returnPiece) && GetComponent<PieceMovement>().canIdle)
+        {
+            transform.position = new Vector2 (piecePosition.x, piecePosition.y + GetComponent<Scroller>().yDiff);
+            GetComponent<PieceMovement>().canIdle = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        piecePosition = transform.position;
+    }
+}
